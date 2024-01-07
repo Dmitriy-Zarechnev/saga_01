acf65dba2788433ca74253aedae0e35651d59004
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-01-07 17:43:42 +0300

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {useEffect, useState} from 'react'
 
 type TagType = {
     id: number,
@@ -10,14 +11,22 @@ type TagsDataType = TagType[]
 
 function App() {
 
-    const tags: TagsDataType = [
-        {id: 1, title: 'css'},
-        {id: 2, title: 'JS'},
-        {id: 3, title: 'React'},
-        {id: 4, title: 'HTML'},
-        {id: 5, title: 'NodeJS'},
-        {id: 6, title: 'SCSS'},
-    ]
+
+    const [tags, setTags] = useState<TagsDataType>([])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTags([
+                {id: 1, title: 'css'},
+                {id: 2, title: 'JS'},
+                {id: 3, title: 'React'},
+                {id: 4, title: 'HTML'},
+                {id: 5, title: 'NodeJS'},
+                {id: 6, title: 'SCSS'}
+            ])
+        }, 2000)
+    }, [])
+
 
     return (
         <div>
